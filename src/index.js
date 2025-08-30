398bc176d03eeabdaa540da9b78239ec072579f9
Lautaro-Espinosa015 <espinosalautii@outlook.com>
2025-08-30 13:40:53 -0300

    css se encuentra en una carpeta separada + logica para no ingresar textos en el campo numerico - EspLau

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ document.getElementById("calcular").addEventListener("click", function() {
     let suma = numero1 * numero2;
     let nombre = document.getElementById("nombre").value;
 
+     if (isNaN(numero1) || isNaN(numero2)) {
+        document.getElementById("resultado").innerHTML = "⚠️ Por favor, ingresá solo números.";
+        return;
+    }
+
+    numero1 = Number(numero1);
+    numero2 = Number(numero2);
+    suma = numero1 * numero2;
+
+
     if(numero1>=160){
         let extra = suma*0.20;
         suma = suma+extra;
